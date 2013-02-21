a3ac2a05ac0dbff3f7fd8406c98f56d8656efe95
Pradeep Nayak <pradeep1288@gmail.com>
2013-02-20 20:46:29 -0800

    created a json of amazon ratings

--- gettherating.py
+++ gettherating.py
@@ -3,6 +3,7 @@
 from BeautifulSoup import BeautifulSoup
 import re
 import urllib2
+import json
 
 product_rating = dict()
 rating  = []
@@ -12,6 +13,7 @@ def get_rating_from_amazon(url, product):
 
     product_summary_table = soup.find('table',id="productSummary")
     try:
+        rating = []
         product_summary_table_entries = product_summary_table.find('tr').findAll('td')
         for i in range(1, 6):
             j = 3 * i
@@ -21,9 +23,11 @@ def get_rating_from_amazon(url, product):
     except Exception, e:
         print e
 
-url = "http://www.amazon.com/Apple-iPhone-16GB-White-Unlocked/product-reviews/B0097CZJEO/"
-get_rating_from_amazon(url, "iphone")
-print product_rating["iphone"]
+url_iphone = "http://www.amazon.com/Apple-iPhone-16GB-White-Unlocked/product-reviews/B0097CZJEO/"
+url_nexus = "http://www.amazon.com/Google-Nexus-Phone-16GB-Unlocked/product-reviews/B00ABPKHH0/"
+get_rating_from_amazon(url_iphone, "iphone")
+get_rating_from_amazon(url_nexus, "nexus4")
 
+print json.dumps(product_rating)
 
 
