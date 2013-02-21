68cf5f7d157d2363bf6c75a68eea43636f706210
Pradeep Nayak <pradeep1288@gmail.com>
2013-02-20 20:15:20 -0800

    built a dictionary to store rating values

--- gettherating.py
+++ gettherating.py
@@ -5,22 +5,25 @@ import re
 import urllib2
 
 product_rating = dict()
+rating  = []
 def get_rating_from_amazon(url, product):
     content = urllib2.urlopen(url)
     soup = BeautifulSoup(content)
 
     product_summary_table = soup.find('table',id="productSummary")
-    myregex = '<td align=\"right\" style=\"font-family\:Verdana\,Arial\,Helvetica,Sans-serif;;font-size:10px;\">\&nbsp\;(.*)</td>'
     try:
         product_summary_table_entries = product_summary_table.find('tr').findAll('td')
         for i in range(1, 6):
             j = 3 * i
-            print product_summary_table_entries[j].renderContents().replace('&nbsp;','').replace('(', '').replace(')','')
+            rating.append(product_summary_table_entries[j].renderContents().replace('&nbsp;','').replace('(', '').replace(')',''))
+        rating.reverse()
+        product_rating[product] = rating
     except Exception, e:
         print e
 
 url = "http://www.amazon.com/Apple-iPhone-16GB-White-Unlocked/product-reviews/B0097CZJEO/"
 get_rating_from_amazon(url, "iphone")
+print product_rating["iphone"]
 
 
 
